a2ad1d7492e216795ed98db16fb9546913d4dd05
tomokisato <tomoki.sato@hacobu.jp>
2023-01-12 01:09:51 +0900

    めくるボタンを押したときに定義済みリストからランダムに内容を取得できるようにした

---
+++ main.js
@@ -0,0 +1,11 @@
+const getAphorism = (aphorisms) => {
+  return aphorisms[Math.floor(Math.random() * aphorisms.length)];
+}
+
+const aphorism = document.querySelector(".aphorism_text");
+const pager = document.querySelector("#pager");
+const aphorisms = ["a", "i", "u", "e", "o"];
+
+pager.addEventListener("click", () => {
+  aphorism.innerHTML = getAphorism(aphorisms);
+});
